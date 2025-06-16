084dd2cf29cb93206da7db751a233701b428a3c0
psmao666 <pengsen.mao88@gmail.com>
2025-06-16 18:46:54 +1000

    refactor: change api key to local storage

--- exchange/binance/binance.cpp
+++ exchange/binance/binance.cpp
@@ -10,10 +10,15 @@ namespace degen_crypto { namespace exchange { namespace binance {
 
 auto BinanceExchange::on_start() -> bool {
     fmt::print("BinanceExchange::on_start()\n");
-    std::ifstream config_file("exchange/binance/config.json");
+    std::ifstream config_file("exchange/binance/api_key.json");
     nlohmann::json config;
     config_file >> config;
     init_account_api_config(config["api_key"], config["secret_key"]);
+    config_file.close();
+    
+    config_file.open("exchange/binance/config.json");
+    config_file >> config;
+    config_file.close();
     init_exchange_hosts(config["hosts"]);
     if (!ping_exchange()) {
         fmt::print("Binance bootstrap failed due to ping exchange failure\n");
