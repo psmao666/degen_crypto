f8010c5b13d404f52b3a691bd001170fda31f0a7
psmao666 <pengsen.mao88@gmail.com>
2025-07-31 19:10:10 +1000

    fix: bugfix, also added errors log fetcher

--- kitchen/arbitrage/fdusd_usdt_grid.h
+++ kitchen/arbitrage/fdusd_usdt_grid.h
@@ -66,7 +66,7 @@ public:
         if (best_bid >= config_.sell_levels[0].price && fdusd > 6) {
             const auto quantity = static_cast<int>(fdusd);
             LOG_INFO(logger::g_logger, "{}::bid level hit at {}, current usdt: {}, current usdc: {}", strategy_name(), best_bid, usdt, fdusd);
-            if (engine.trade(exchange::binance::constants::USDC_USDT, 
+            if (engine.trade(exchange::binance::constants::FDUSD_USDT, 
                             exchange::binance::Enums::OrderSide::SELL, 
                             exchange::binance::Enums::OrderType::MARKET, 
                             std::to_string(quantity),
@@ -76,7 +76,7 @@ public:
                     portfolio.sub_position(FDUSD, quantity);
                     usdt += quantity * best_bid;
                     fdusd -= quantity;
-                    LOG_INFO(logger::g_logger, "{}::Sell USDC Order filled, current usdt: {}, current usdc: {}", strategy_name(), usdt, fdusd);
+                    LOG_INFO(logger::g_logger, "{}::Sell USDC Order filled, current usdt: {}, current fdusd: {}", strategy_name(), usdt, fdusd);
             }
             else {
                 LOG_ERROR(logger::g_logger, "{}::Order failed to fill", strategy_name());
